bd8e45af4883e08a1c9b062980a714a59d1cb907
iskndrr <mohhamed.a.saeed@gmail.com>
2024-12-01 12:44:44 +0200

    first commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,36 @@
+import type { Metadata } from "next";
+import { Inter, Calistoga } from "next/font/google";
+import "./globals.css";
+import { twMerge } from "tailwind-merge";
+
+const inter = Inter({ subsets: ["latin"], variable: "--font-sans" });
+const calisoga = Calistoga({
+  subsets: ["latin"],
+  variable: "--font-serif",
+  weight: ["400"],
+});
+
+export const metadata: Metadata = {
+  title: "Mohamed Saeed Portfolio",
+  description: "Angular/React Frontend Developer and you can call me Iskndr by the way;)",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={twMerge(
+          inter.variable,
+          calisoga.variable,
+          "bg-gray-900 text-white antialiased font-sans "
+        )}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
